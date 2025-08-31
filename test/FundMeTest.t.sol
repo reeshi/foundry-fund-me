1f85c6c4b4fc83a49a6f26297b228a6f160e8f10
Rishikesh Yadav <yadavrishikesh53@gmail.com>
2025-08-31 19:16:10 +0530

    test

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -9,11 +9,16 @@ contract FundMeTest is Test {
     FundMe fundMe;
     DeployFundMe deployFundMe;
 
+    address USER = makeAddr("USER"); // Make the address
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
+
     // setUp function always run first
     function setUp() external {
         // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE); // giving the fake user , fake money
     }
 
     function testMinimumDollarIsFive() public view {
@@ -27,5 +32,35 @@ contract FundMeTest is Test {
     function testPriceFeedVersionIsAccurate() public view {
         uint256 version = fundMe.getVersion();
         assertEq(version, 4);
+    }
+
+    function testFundFailsWithoutEnoughEth() public {
+        vm.expectRevert(); // the next TX should revert.
+        fundMe.fund(); // send 0 value
+    }
+
+    function testFundeUpdatesFundedDataStuctures() public {
+        vm.prank(USER); // The next TX will be sent by USER
+        fundMe.fund{value: SEND_VALUE}();
+
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
+    }
+
+    function testAddsFunderToArrayOfFunders() public {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+
+        address funder = fundMe.getFunder(0);
+        assertEq(funder, USER);
+    }
+
+    function testOnlyOwnerCanWithdraw() public {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+
+        vm.expectRevert();
+        vm.prank(USER);
+        fundMe.withdraw();
     }
 }
